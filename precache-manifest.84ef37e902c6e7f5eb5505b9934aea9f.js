adab07ca2823313a5f073f21ce814635821cec66
Ramkrishna <psiddhesh14@gmail.com>
2019-06-24 19:37:11 +0530

    Updates

---
+++ precache-manifest.84ef37e902c6e7f5eb5505b9934aea9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262aa3ffd6f7f343e992e028a46b5ca0",
+    "url": "/sptfrontend/index.html"
+  },
+  {
+    "revision": "a24c27e34b93f0eb7910",
+    "url": "/sptfrontend/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/sptfrontend/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a24c27e34b93f0eb7910",
+    "url": "/sptfrontend/static/js/main.d3ad6c69.chunk.js"
+  },
+  {
+    "revision": "0d4eac67d7fffe38cd32",
+    "url": "/sptfrontend/static/js/runtime~main.532f9c32.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sptfrontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
